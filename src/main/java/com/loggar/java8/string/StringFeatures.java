a49a10921e78b936265054fa5bd85d6d22065a30
loggar.lee <loggar.lee@DT-Charly.koi.local>
2017-11-14 14:26:18 +1100

    join, toarray

---
+++ src/main/java/com/loggar/java8/string/StringFeatures.java
@@ -0,0 +1,12 @@
+package com.loggar.java8.string;
+
+import org.junit.Test;
+
+public class StringFeatures {
+	@Test
+	public void join() {
+		String [] arr = {"a", "b", "c"};
+		String s = String.join(", ", arr);
+		System.out.println(s);
+	}
+}
