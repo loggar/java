1806d125f487393ae6240c04917b29fc248adb7d
loggar.lee <loggar.lee@DT-Charly.koi.local>
2017-11-22 17:48:48 +1100

    http response header

--- src/main/java/com/loggar/http/connection/GetHTTPContentType.java
+++ src/main/java/com/loggar/http/connection/GetHTTPContentType.java
@@ -1,4 +1,4 @@
-package com.loggar.http.entity;
+package com.loggar.http.connection;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
---
+++ src/main/java/com/loggar/http/connection/HTTPResponseHeader.java
@@ -0,0 +1,33 @@
+package com.loggar.http.connection;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class HTTPResponseHeader {
+	public static void main(String[] args) {
+		try {
+			URL obj = new URL("http://google.com");
+			URLConnection conn = obj.openConnection();
+			Map<String, List<String>> map = conn.getHeaderFields();
+ 
+			System.out.println("Printing All Response Header for URL: " + obj.toString() + "\n");
+			for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+			}
+			
+			System.out.println("\nGet Response Header By Key ...\n");
+			List<String> contentLength = map.get("Content-Length");
+			if (contentLength == null) {
+				System.out.println("'Content-Length' doesn't present in Header!");
+			} else {
+				for (String header : contentLength) {
+					System.out.println("Content-Lenght: " + header);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
