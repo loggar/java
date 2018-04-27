2dab733444030cc87400ff57fabba4b5c2ad14c0
charly.lee <charly.lee@DT-Charly.koi.local>
2018-04-27 17:38:54 +1000

    json web token

--- src/main/java/com/loggar/jwt/jsonwebtoken/JsonWebTokenTest.java
+++ src/main/java/com/loggar/jwt/jsonwebtoken/JsonWebTokenTest.java
@@ -12,11 +12,11 @@ public class JsonWebTokenTest {
 		long expireMillis = 60 * 1000;
 
 		JsonWebToken jwt = new JsonWebToken();
-		String token1 = jwt.create("3ee9364e-8aca-4e39-8ba2-74e654c7e083", "https://domain_1.com", "user1234",
+		String token1 = jwt.create("3ee9364e-8aca-4e39-8ba2-74e654c7e083", "https://domain_1.com", "forCommunicateWithSomething",
 				expireMillis);
 		System.out.println(token1);
 
-		String token2 = jwt.create("loggar", "https://domain_2.com", "user5678", expireMillis);
+		String token2 = jwt.create("loggar", "https://domain_2.com", "forCommunicateWithSomething", expireMillis);
 		System.out.println(token2);
 
 		Claims claims1 = jwt.verify(token1);
