e9d8f92ba98ac54eeb2f451a7b52400f3235cbd4
charly.lee <charly.lee@DT-Charly.koi.local>
2018-05-14 15:08:40 +1000

    DirectoryStream

---
+++ src/main/java/com/loggar/io/nio2/FilesList.java
@@ -0,0 +1,29 @@
+package com.loggar.io.nio2;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FilesList {
+	@Test
+	public void test_fileList() {
+		System.out.println(fileList("./dist/scriptengines-js"));
+	}
+	
+	public List<String> fileList(String dir) {
+		List<String> fileNames = new ArrayList<>();
+		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(dir))) {
+			for (Path path : directoryStream) {
+				fileNames.add(path.toString());
+			}
+		} catch (IOException ex) {
+		}
+		return fileNames;
+	}
+}
