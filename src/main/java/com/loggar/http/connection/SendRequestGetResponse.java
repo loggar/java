a50945b5151bb9dc33da9d9e5fc90b7d70f5b481
charly.lee <charly.lee@DT-Charly.koi.local>
2018-06-12 14:36:45 +1000

    SendRequestGetResponse

--- src/main/java/com/loggar/http/connection/SendRequestGetResponse.java
+++ src/main/java/com/loggar/http/connection/SendRequestGetResponse.java
@@ -10,10 +10,14 @@ public class SendRequestGetResponse {
 	public static void main(String[] args) {
 		String url = "http://www.mocky.io/v2/5a165cd03100004f008d31ee";
 		System.out.println("Requeted URL:" + url);
-		System.out.println("Output: \n" + sendRequest(url));
+		System.out.println("Output: \n" + sendRequest(url, 1000 * 2));
+
+		String url2 = "https://loggar.github.io/js/docs/sample-res/sample.1.json";
+		System.out.println("Requeted URL:" + url2);
+		System.out.println("Output: \n" + sendRequest(url2, 1000 * 2));
 	}
 
-	public static String sendRequest(String myURL) {
+	public static String sendRequest(String myURL, int timeout) {
 
 		StringBuilder sb = new StringBuilder();
 		URLConnection urlConn = null;
@@ -22,7 +26,7 @@ public class SendRequestGetResponse {
 			URL url = new URL(myURL);
 			urlConn = url.openConnection();
 			if (urlConn != null)
-				urlConn.setReadTimeout(60 * 1000);
+				urlConn.setReadTimeout(timeout);
 			if (urlConn != null && urlConn.getInputStream() != null) {
 				in = new InputStreamReader(urlConn.getInputStream(), Charset.defaultCharset());
 				BufferedReader bufferedReader = new BufferedReader(in);
