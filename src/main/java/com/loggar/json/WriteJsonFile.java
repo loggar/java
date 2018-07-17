8727dfce567ef338614e1a6da6ae81ab50d62988
charly.lee <charly.lee@DT-Charly.koi.local>
2018-07-17 10:32:30 +1000

    json write file

--- src/main/java/com/loggar/json/WriteJsonFile.java
+++ src/main/java/com/loggar/json/WriteJsonFile.java
@@ -15,23 +15,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class WriteJsonFile {
-	// @Test
-	public void write() throws UnsupportedEncodingException, FileNotFoundException, IOException {
-		String s = "./dist/json/sample.1.json";
-
-		Map<String, Object> map = new HashMap<>();
-		map.put("status", "processing");
-		map.put("file", "sample.xlsx");
-		map.put("insUid", "charly");
-		map.put("email", "sample@domain.com");
-		map.put("reportDate", "11/07/2018");
-
-		try (Writer writer = new OutputStreamWriter(new FileOutputStream(s), "UTF-8")) {
-			Gson gson = new GsonBuilder().create();
-			gson.toJson(map, writer);
-		}
-	}
-
 	@Test
 	public void write_stream() throws UnsupportedEncodingException, FileNotFoundException, IOException {
 		String s = "./dist/json/sample.1.json";
