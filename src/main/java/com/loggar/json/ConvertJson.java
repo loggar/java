1a6618824035f491745622420aadab2a9f8fe7c9
charly.lee <charly.lee@DT-Charly.koi.local>
2018-08-29 15:28:14 +1000

    feat (json) convert from json

--- src/main/java/com/loggar/json/ConvertJson.java
+++ src/main/java/com/loggar/json/ConvertJson.java
@@ -1,6 +1,7 @@
 package com.loggar.json;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,10 +13,20 @@ import com.google.gson.reflect.TypeToken;
 
 public class ConvertJson {
 	@Test
-	public void jsonToObject() {
+	public void jsonToList1() {
 		String json = "[{'1':'a_1'},{'2':'a_2'}]";
 		List<Map<String, Object>> list = (new Gson()).fromJson(json, new TypeToken<List<Map<String, Object>>>() {
 		}.getType());
+		System.out.println(list);
+	}
+
+	@Test
+	public void jsonToList2() {
+		Gson gson = new Gson();
+		String json = "[{'1':'a_1'},{'2':'a_2'}]";
+		Map<String, Object>[] arr = gson.fromJson(json, new TypeToken<Map<String, Object>[]>() {
+		}.getType());
+		List<Map<String, Object>> list = Arrays.asList(arr);
 		System.out.println(list);
 	}
 
