6809d3b9e8653f8ec3754cac844e1c6258679dff
charly.lee <charly.lee@DT-Charly.koi.local>
2018-09-03 13:26:57 +1000

    feat (json) convert to object

--- src/main/java/com/loggar/json/ConvertJson.java
+++ src/main/java/com/loggar/json/ConvertJson.java
@@ -12,6 +12,48 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class ConvertJson {
+	class SomeObj {
+		private String a;
+		private String b;
+		
+		public String getA() {
+			return a;
+		}
+		public void setA(String a) {
+			this.a = a;
+		}
+		public String getB() {
+			return b;
+		}
+		public void setB(String b) {
+			this.b = b;
+		}
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("SomeObj [a=").append(a).append(", b=").append(b).append("]");
+			return builder.toString();
+		}
+	}
+	
+	@Test
+	public void objectToJson() {
+		List<Map<String, Object>> list = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+		map.put("a", "b");
+		list.add(map);
+
+		String json = (new Gson()).toJson(list);
+		System.out.println(json);
+	}
+	
+	@Test
+	public void jsonToObject() {
+		String json = "{'a':'a_1','b':'b_1'}";
+		SomeObj obj = (new Gson()).fromJson(json, SomeObj.class);
+		System.out.println(obj);
+	}
+	
 	@Test
 	public void jsonToList1() {
 		String json = "[{'1':'a_1'},{'2':'a_2'}]";
@@ -30,14 +72,4 @@ public class ConvertJson {
 		System.out.println(list);
 	}
 
-	@Test
-	public void objectToJson() {
-		List<Map<String, Object>> list = new ArrayList<>();
-		Map<String, Object> map = new HashMap<>();
-		map.put("a", "b");
-		list.add(map);
-
-		String json = (new Gson()).toJson(list);
-		System.out.println(json);
-	}
 }
