c0db5eb56ff01d31dea2f3757c86de99302ab5c1
charly.lee <webnl@DT-Charly.koi.local>
2019-04-09 13:31:24 +1000

    feat (functional)

--- src/main/java/com/loggar/statements/FluentApis.java
+++ src/main/java/com/loggar/statements/FluentApis.java
@@ -10,25 +10,21 @@ public class FluentApis {
 	@Test
 	public void fluentApiModuleSample_1() {
 		StringBuilder sb = new StringBuilder("abcdefg hijklmn");
-		
+
 		// method chaining
-		sb.delete(10,  11).append("st").reverse();
-		
+		sb.delete(10, 11).append("st").reverse();
+
 		System.out.println(sb);
 	}
-	
+
 	@Test
 	public void fluentApiModuleSample_2() {
 		LocalDateTime ldt1 = LocalDateTime.now();
 		System.out.println(ldt1);
-		
+
 		// static method with method chaining
-		LocalDateTime ldt2 = LocalDateTime.now()
-				.withDayOfMonth(Calendar.TUESDAY)
-				.withYear(1981)
-				.plusWeeks(2)
-				.minus(3, ChronoUnit.HOURS);
+		LocalDateTime ldt2 = LocalDateTime.now().withDayOfMonth(Calendar.TUESDAY).withYear(1981).plusWeeks(2).minus(3, ChronoUnit.HOURS);
 		System.out.println(ldt2);
-		
+
 	}
 }
