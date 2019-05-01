ec0287c8b2ab4fbd36065ac437cdd42a67307177
charly.lee <webnl@DT-Charly.koi.local>
2019-05-01 16:25:18 +1000

    feat (string) util

--- src/main/java-tools/com/loggar/tools/common/StringUtil.java
+++ src/main/java-tools/com/loggar/tools/common/StringUtil.java
@@ -17,8 +17,7 @@ public class StringUtil {
 	}
 
 	public static String toString(String[] arr_s, String delimiter) {
-		if (isEmpty(delimiter))
-			delimiter = default_array_delimiter;
+		if (isEmpty(delimiter)) delimiter = default_array_delimiter;
 
 		StringBuffer sb = new StringBuffer();
 		sb.setLength(0);
@@ -48,32 +47,26 @@ public class StringUtil {
 	}
 
 	public static boolean isEmpty(String target) {
-		if (target == null)
-			return true;
-		if (target.trim().length() == 0)
-			return true;
+		if (target == null) return true;
+		if (target.trim().length() == 0) return true;
 		return false;
 	}
 
 	public static boolean isEmpty(Object t) {
-		if (t == null)
-			return true;
+		if (t == null) return true;
 		String target;
 		try {
 			target = (String) t;
 		} catch (Exception e) {
 			return true;
 		}
-		if (target.trim().length() == 0)
-			return true;
+		if (target.trim().length() == 0) return true;
 		return false;
 	}
 
 	public static boolean isNotEmpty(String target) {
-		if (target == null)
-			return false;
-		if (target.trim().length() < 1)
-			return false;
+		if (target == null) return false;
+		if (target.trim().length() < 1) return false;
 		return true;
 	}
 
@@ -86,10 +79,8 @@ public class StringUtil {
 	}
 
 	public static boolean isEmptyOrStrNull(String s) {
-		if (isEmpty(s))
-			return true;
-		if ("null".equals(s.trim().toLowerCase()))
-			return true;
+		if (isEmpty(s)) return true;
+		if ("null".equals(s.trim().toLowerCase())) return true;
 		return false;
 	}
 
@@ -103,35 +94,19 @@ public class StringUtil {
 		StringBuffer sb = new StringBuffer();
 		sb.append("{");
 		for (K key : keySet) {
-			sb.append(key).append("=").append(map.get(key)).append(", ");
+			sb.append(SYSTEM_LINE_SEPARATER).append("  ").append(key).append(": ").append(map.get(key)).append(", ");
 		}
 
 		String result = sb.toString();
 		if (result.length() > 2) {
-			return result.substring(0, result.length() - 2) + "}";
+			return result.substring(0, result.length() - 2) + SYSTEM_LINE_SEPARATER + "}";
 		} else {
 			return "{}";
 		}
 	}
 
 	public static <T> String debugList(List<T> list) {
-		if (list == null || list.size() < 1) {
-			return "[]";
-		}
-
-		StringBuffer sb = new StringBuffer();
-		sb.append("[");
-
-		for (T e : list) {
-			sb.append(e.toString());
-		}
-
-		String result = sb.toString();
-		if (result.length() > 2) {
-			return result.substring(0, result.length() - 2) + "]";
-		} else {
-			return "[]";
-		}
+		return debugList(list, false);
 	}
 
 	public static <T> String debugList(List<T> list, boolean newLine) {
@@ -141,16 +116,19 @@ public class StringUtil {
 
 		StringBuffer sb = new StringBuffer();
 		sb.append("[");
+		if (newLine) sb.append(SYSTEM_LINE_SEPARATER);
 
 		for (T e : list) {
 			sb.append(e.toString());
-			if (newLine)
-				sb.append(SYSTEM_LINE_SEPARATER);
+			if (newLine) sb.append(SYSTEM_LINE_SEPARATER);
 		}
 
-		String result = sb.toString();
-		if (result.length() > 2) {
-			return result.substring(0, result.length() - 2) + "]";
+		String r = sb.toString();
+		if (r.length() > 2) {
+			String r2 = r.substring(0, r.length() - 2);
+			if (newLine) r2 = r2 + SYSTEM_LINE_SEPARATER;
+			return r2 + "]";
+
 		} else {
 			return "[]";
 		}
@@ -160,8 +138,7 @@ public class StringUtil {
 		StringBuilder sb = new StringBuilder();
 		sb.setLength(0);
 		for (String s : a) {
-			if (sb.length() > 0)
-				sb.append(delimiter);
+			if (sb.length() > 0) sb.append(delimiter);
 			sb.append(s);
 		}
 		return sb.toString();
@@ -171,8 +148,7 @@ public class StringUtil {
 		StringBuilder sb = new StringBuilder();
 		sb.setLength(0);
 		for (String s : a) {
-			if (sb.length() > 0)
-				sb.append(delimiter);
+			if (sb.length() > 0) sb.append(delimiter);
 			sb.append(s);
 		}
 		return sb.toString();
@@ -191,6 +167,68 @@ public class StringUtil {
 			return (char) ((int) c - 32);
 		} else {
 			return c;
+		}
+	}
+
+	/**
+	 * <pre>
+	 * Fill Zero value
+	 * zeroFill(1380,7) = 0001380
+	 * </pre>
+	 *
+	 * @param seqno
+	 * @param size
+	 * @return
+	 */
+	public static String zeroFill(int seqno, int size) {
+		StringBuffer convert = new StringBuffer();
+		int maxSize = (int) Math.pow(10, size); // if size=5 then maxSize=100,000
+
+		if (seqno >= maxSize) {
+			convert.append(Integer.toString(maxSize - 1));
+		} else {
+			int seqnoSize = (Integer.toString(seqno)).length();
+			for (int i = 0; i < (size - seqnoSize); i++) {
+				convert.append("0");
+			}
+			convert.append(seqno);
+		}
+		return convert.toString();
+	}
+
+	public static boolean isNumeric(String str) {
+		return str.matches("-?\\d+(\\.\\d+)?");
+	}
+
+	public static int parseInt(String arg) {
+		if (isEmpty(arg) || !isNumeric(arg)) {
+			return 0;
+		} else {
+			return Integer.parseInt(arg);
+		}
+	}
+
+	public static double parseDouble(String arg) {
+		if (isEmpty(arg) || !isNumeric(arg)) {
+			return 0;
+		} else {
+			return Double.parseDouble(arg);
+		}
+	}
+
+	public static long parseLong(String arg) {
+		if (isEmpty(arg) || !isNumeric(arg)) {
+			return 0;
+		} else {
+			return Long.parseLong(arg);
+		}
+	}
+
+	public static float parseFloat(String arg) {
+		if (isEmpty(arg) || !isNumeric(arg)) {
+			return 0;
+		} else {
+			return Float.parseFloat(arg);
 		}
 	}
 }
