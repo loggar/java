8b115e58042d26625ce16b9930ca889403be3d80
charly.lee <webnl@DT-Charly.koi.local>
2019-07-15 14:28:17 +1000

    feat (line-separator)

--- src/main/java/com/loggar/strings/SystemLineSeparator.java
+++ src/main/java/com/loggar/strings/SystemLineSeparator.java
@@ -2,6 +2,7 @@ package com.loggar.strings;
 
 public class SystemLineSeparator {
 	private static final String SYSTEM_LINE_SEPARATER = System.getProperty("line.separator");
+	private static final String SYSTEM_NEW_LINE = System.lineSeparator(); // java 1.7 or later
 
 	public static void main(String[] args) {
 		System.out.println("Hearing in the distance");
@@ -11,7 +12,7 @@ public class SystemLineSeparator {
 		System.out.println("dark");
 		System.out.print(SYSTEM_LINE_SEPARATER + SYSTEM_LINE_SEPARATER);
 		System.out.println("Creating the agony of ecstasy.");
-		System.out.print(SYSTEM_LINE_SEPARATER + SYSTEM_LINE_SEPARATER);
+		System.out.print(SYSTEM_NEW_LINE + SYSTEM_NEW_LINE);
 		System.out.println("                - George Barker");
 	}
 
