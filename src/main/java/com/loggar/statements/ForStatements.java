233b88639881c6b2e087f2a9c211461fd01401cb
charly.lee <webnl@DT-Charly.mshome.net>
2019-09-06 10:24:18 +1000

    feat (for)

--- src/main/java/com/loggar/statements/ForStatements.java
+++ src/main/java/com/loggar/statements/ForStatements.java
@@ -4,9 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class CountForBody {
+public class ForStatements {
     @Test
     public void countForBody() {
+        // How many times will the following for loop be executed?
         int i = 0;
         for (int count = 10; count <= 21; count++) {
             System.out.println("Java is great!!!");
