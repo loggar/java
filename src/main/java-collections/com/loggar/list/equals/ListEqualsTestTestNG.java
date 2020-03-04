45684b75023ba874a69642e3aeff3cec202f5107
Charly LEE <charly.loggar@gmail.com>
2020-03-04 15:17:49 +1100

    refactor (list) equals

--- src/main/java-collections/com/loggar/list/equals/ListEqualsTestTestNG.java
+++ src/main/java-collections/com/loggar/list/equals/ListEqualsTestTestNG.java
@@ -1,21 +1,20 @@
-package com.loggar.list;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+package com.loggar.list.equals;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class ListEqualsTestJUnit {
+public class ListEqualsTestTestNG {
 	@Test
 	public void whenTestingForEquality_ShouldBeEqual() throws Exception {
 		List<String> list1 = Arrays.asList("1", "2", "3", "4");
 		List<String> list2 = Arrays.asList("1", "2", "3", "4");
 		List<String> list3 = Arrays.asList("1", "2", "4", "3");
 
-		assertTrue(list1.equals(list2));
-		assertFalse(list1.equals(list3));
+		Assert.assertEquals(list1, list2);
+		Assert.assertNotSame(list1, list2);
+		Assert.assertNotEquals(list1, list3);
 	}
 }
