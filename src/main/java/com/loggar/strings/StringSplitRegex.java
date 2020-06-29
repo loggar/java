46036321c82c3139bf4b57235d066984b446dbba
Charly LEE <charly.loggar@gmail.com>
2020-06-29 17:20:11 +1000

    feat (string) split

---
+++ src/main/java/com/loggar/strings/StringSplitRegex.java
@@ -0,0 +1,10 @@
+package com.loggar.strings;
+
+public class StringSplitRegex {
+	public static void main(String [] args) {
+		String str = "34,36,14,";
+		String [] a = str.split(",");
+		System.out.println(a.length);
+	}
+}
+
