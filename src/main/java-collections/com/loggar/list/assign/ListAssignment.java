970cb51a55abc917453ff38a7c43fe39c3fbb7cc
Charly LEE <charly.loggar@gmail.com>
2020-11-16 16:04:42 +1100

    feat (list) nCopies

--- src/main/java-collections/com/loggar/list/assign/ListAssignment.java
+++ src/main/java-collections/com/loggar/list/assign/ListAssignment.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,51 +15,56 @@ import java.util.stream.LongStream;
 import org.junit.jupiter.api.Test;
 
 public class ListAssignment {
-    @Test
-    public void isEmpry() {
-        List<String> list = new ArrayList<>();
-        assertTrue(list.isEmpty());
-    }
+	@Test
+	public void isEmpry() {
+		List<String> list = new ArrayList<>();
+		assertTrue(list.isEmpty());
+	}
 
-    @Test
-    public void initCapacity() throws Exception {
-        int initSize = 20;
-        List<String> list = new ArrayList<>(initSize);
-        System.out.println(list.size());
-        assertTrue(list.size() == 0);
-        assertTrue(list.isEmpty());
-        assertTrue(ArrayListCapacity.get((ArrayList<String>) list) == initSize);
-    }
-    
-    @Test
-    public void initFromCollection() {
-        Collection<Integer> numbers = IntStream.range(0, 10)
-                .boxed()
-                .collect(Collectors.toSet());
+	@Test
+	public void initCapacity() throws Exception {
+		int initSize = 20;
+		List<String> list = new ArrayList<>(initSize);
+		System.out.println(list.size());
+		assertTrue(list.size() == 0);
+		assertTrue(list.isEmpty());
+		assertTrue(ArrayListCapacity.get((ArrayList<String>) list) == initSize);
+	}
 
-        List<Integer> list = new ArrayList<>(numbers);
-        assertEquals(10, list.size());
-        assertTrue(numbers.containsAll(list));
-    }
+	@Test
+	public void initWithNCopy() {
+		List<String> list = Collections.nCopies(10, "HELLO");
+		assertTrue(list.size() == 10);
+		for (String item : list) {
+			assertTrue("HELLO".equals(item));
+		}
+	}
 
-    @Test
-    public void add1() {
-        List<Long> list = new ArrayList<>();
+	@Test
+	public void initFromCollection() {
+		Collection<Integer> numbers = IntStream.range(0, 10).boxed().collect(Collectors.toSet());
 
-        list.add(1L);
-        list.add(2L);
-        list.add(1, 3L);
+		List<Integer> list = new ArrayList<>(numbers);
+		assertEquals(10, list.size());
+		assertTrue(numbers.containsAll(list));
+	}
 
-        assertEquals(Arrays.asList(1L, 3L, 2L), list);
-    }
+	@Test
+	public void add1() {
+		List<Long> list = new ArrayList<>();
 
-    @Test
-    public void add2() {
-        List<Long> list = new ArrayList<>(Arrays.asList(1L, 2L, 3L));
-        LongStream.range(4, 10)
-                .boxed()
-                .collect(Collectors.collectingAndThen(Collectors.toCollection(ArrayList::new), ys -> list.addAll(0, ys)));
-        assertEquals(Arrays.asList(4L, 5L, 6L, 7L, 8L, 9L, 1L, 2L, 3L), list);
-    }
+		list.add(1L);
+		list.add(2L);
+		list.add(1, 3L);
+
+		assertEquals(Arrays.asList(1L, 3L, 2L), list);
+	}
+
+	@Test
+	public void add2() {
+		List<Long> list = new ArrayList<>(Arrays.asList(1L, 2L, 3L));
+		LongStream.range(4, 10).boxed().collect(Collectors.collectingAndThen(Collectors.toCollection(ArrayList::new), ys -> list.addAll(0, ys)));
+		assertEquals(Arrays.asList(4L, 5L, 6L, 7L, 8L, 9L, 1L, 2L, 3L), list);
+	}
 
 }
