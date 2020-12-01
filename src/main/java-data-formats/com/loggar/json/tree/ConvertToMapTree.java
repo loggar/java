97af4374f4ce58e1640ab69d7adda16e5a47aae5
Charly LEE <charly.loggar@gmail.com>
2020-12-01 14:54:35 +1100

    feat (tree) json

--- src/main/java-data-formats/com/loggar/json/tree/ConvertToMapTree.java
+++ src/main/java-data-formats/com/loggar/json/tree/ConvertToMapTree.java
@@ -1,7 +1,9 @@
 package com.loggar.json.tree;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,25 +16,22 @@ import com.loggar.structure.tree.TraverseTreeIterative;
 import com.loggar.structure.tree.TreeNode;
 
 public class ConvertToMapTree {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		try {
-			Gson gson = new Gson();
 			String jsonString = FileUtils.readFileToString(new File("./dist/json/sample.2.json"), "UTF-8");
 
-			Map<String, Object> map = gson.fromJson(jsonString, new TypeToken<Map<String, Object>>() {
+			Map<String, Object> map = (new Gson()).fromJson(jsonString, new TypeToken<Map<String, Object>>() {
 			}.getType());
 
 			// System.out.println(map.getClass().toString());
-			System.out.println(map);
+			// System.out.println(map);
 
-			LinkedTreeMap<String, Object> properties = (LinkedTreeMap<String, Object>) map;
+			System.out.println("spec-name: " + "some spec name");
+			System.out.println("url: " + map.get("url"));
 
-			TreeNode<Map<String, Object>> propsTreeRoot = treeMaker(properties, null, null);
-			// treeMaker(map, specTreeRoot);
-
+			TreeNode<Map<String, Object>> propsTreeRoot = treeMaker((LinkedTreeMap<String, Object>) map);
 			TraverseTreeIterative.traversePreOrder(propsTreeRoot);
-
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -41,7 +40,26 @@ public class ConvertToMapTree {
 		return !"object".equals(t) && !"list".equals(t);
 	}
 
-	// tmp view for treeMaker?
+	/**
+	 * Make a tree for the given json spec
+	 * 
+	 * @param e
+	 * @return
+	 */
+	public static TreeNode<Map<String, Object>> treeMaker(LinkedTreeMap<String, Object> e) {
+		TreeNode<Map<String, Object>> rootNode = treeMaker(e, null, null);
+		traverseSetInfoType(rootNode, "root");
+		return rootNode;
+	}
+
+	/**
+	 * Tree Factory for a json spec
+	 * 
+	 * @param e
+	 * @param pk
+	 * @param parent
+	 * @return
+	 */
 	public static TreeNode<Map<String, Object>> treeMaker(LinkedTreeMap<String, Object> e, String pk, TreeNode<Map<String, Object>> parent) {
 		String dataType = (String) e.get("_data-type");
 		if (pk == null) {
@@ -67,6 +85,7 @@ public class ConvertToMapTree {
 			}
 		} else {
 			props.put("_data-type", dataType);
+			props.put("_info-type", (String) e.get("_info-type"));
 			@SuppressWarnings("unchecked") LinkedTreeMap<String, Object> v = (LinkedTreeMap<String, Object>) e.get("properties");
 			Set<String> subKeySet = v.keySet();
 			for (String subKey : subKeySet) {
@@ -76,5 +95,37 @@ public class ConvertToMapTree {
 		}
 
 		return node;
+	}
+
+	/**
+	 * Fill nodes' empty _info-type, Pre order
+	 * 
+	 * @param <T>
+	 * @param node
+	 * @param _infoType
+	 */
+	public static <T> void traverseSetInfoType(TreeNode<Map<String, Object>> node, String _infoType) {
+		if (node == null)
+			return;
+
+		Map<String, Object> v = node.getValue();
+		String infoType = (String) v.get("_info-type");
+
+		System.out.print("visit " + v.get("_data-nm") + " infoType=" + infoType);
+
+		if (infoType != null) {
+			_infoType = infoType;
+			System.out.print(" now _infoType=" + _infoType);
+		} else {
+			v.put("_info-type", _infoType);
+			System.out.print(" update infoType=" + _infoType);
+		}
+
+		System.out.print(System.lineSeparator());
+
+		List<TreeNode<Map<String, Object>>> children = node.getChildren();
+		for (TreeNode<Map<String, Object>> c : children) {
+			traverseSetInfoType(c, _infoType);
+		}
 	}
 }
