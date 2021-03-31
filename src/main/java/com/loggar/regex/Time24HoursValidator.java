628f4faffb6a51b013afbc3776daae75aeac135d
Charly LEE <charly.loggar@gmail.com>
2021-03-31 16:21:47 +1100

    feat (regex) 24hours time

--- src/main/java/com/loggar/regex/Time24HoursValidator.java
+++ src/main/java/com/loggar/regex/Time24HoursValidator.java
@@ -7,7 +7,7 @@ public class Time24HoursValidator {
 	private Pattern pattern;
 	private Matcher matcher;
 
-	private static final String TIME24HOURS_PATTERN = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
+	private static final String TIME24HOURS_PATTERN = "([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
 
 	public Time24HoursValidator() {
 		pattern = Pattern.compile(TIME24HOURS_PATTERN);
@@ -22,5 +22,12 @@ public class Time24HoursValidator {
 	public boolean validate(final String time) {
 		matcher = pattern.matcher(time);
 		return matcher.matches();
+	}
+	
+	public static void main(String [] args) {
+		Time24HoursValidator time24HoursValidator = new Time24HoursValidator();
+		
+		System.out.println(time24HoursValidator.validate("09:00:00"));
+		System.out.println(time24HoursValidator.validate("17:00:00"));
 	}
 }
